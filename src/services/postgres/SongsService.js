6b98311c11915b6ab7c184fb0dc18387c17d3a57
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2023-09-28 19:58:02 +0800

    refactor code

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -14,11 +14,10 @@ class SongsService {
   }) {
     const id = `song-${nanoid(16)}`;
     const createdAt = new Date().toISOString();
-    const updatedAt = createdAt;
 
     const query = {
-      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
-      values: [id, title, year, genre, performer, duration, albumId, createdAt, updatedAt],
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $8) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId, createdAt],
     };
 
     const result = await this._pool.query(query);
@@ -47,7 +46,7 @@ class SongsService {
         `SELECT id, title, performer FROM songs WHERE LOWER(performer) LIKE '%${performer}%'`,
       );
     }
-    return filteredSong.rows.map(mapDBToModel);
+    return filteredSong.rows;
   }
 
   async getSongById(id) {
@@ -57,7 +56,7 @@ class SongsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError('Lagu tidak ditemukan');
     }
 
